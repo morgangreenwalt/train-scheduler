4544e970c33a3fe73b9149c59310b51b48f3f6b4
morgan greenwalt <you@example.com>
2017-05-20 17:32:00 -0500

    attempt at formulas

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -24,6 +24,13 @@ var database = firebase.database();
     var trainTime = $("#train-time").val().trim();
     var frequency = $("#frequency").val().trim();
     var minsAway = "";
+    var convertedTime = moment(trainTime, "HH:MM A");
+    console.log(convertedTime);
+    var trainTimePretty = moment(convertedTime).format("HH:MM A");
+    console.log(trainTimePretty);
+    var nextArrival = moment(convertedTime).diff(moment(), "minutes");
+    console.log(nextArrival);
+    // var minsAway = empMonths * empRate;
 
 //Push variables to Firebase    
     database.ref().push({
@@ -40,7 +47,7 @@ var database = firebase.database();
     var child = snapshot.val();
 
     $(".train-list").prepend("<tr> <td>"+child.trainName+"</td> <td>"+child.destination+"</td> <td>"+child.trainTime+"</td> <td>"+child.frequency+"</td> <td>"+child.minsAway+"</td> </tr>");
-
+    // console.log(convertedTime);
   });
 
 });
