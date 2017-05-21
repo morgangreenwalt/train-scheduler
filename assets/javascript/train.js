dfebd9d73b963fc5d05164e8f72e25c7af1eecd4
morgan greenwalt <you@example.com>
2017-05-21 08:58:32 -0500

    last edits

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -15,39 +15,32 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 //On-click funciton
-  $("#submit-button").on("click", function(event){
+$("#submit-button").on("click", function(event){
     event.preventDefault();
 
 //Train Variables
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var trainTime = $("#train-time").val().trim();
-    var frequency = $("#frequency").val().trim();
-    var minsAway = "";
-    var convertedTime = moment(trainTime, "HH:MM A");
-    console.log(convertedTime);
-    var trainTimePretty = moment(convertedTime).format("HH:MM A");
-    console.log(trainTimePretty);
-    var nextArrival = moment(convertedTime).diff(moment(), "minutes");
-    console.log(nextArrival);
-    // var minsAway = empMonths * empRate;
+  var trainName = $("#train-name").val().trim();
+  var destination = $("#destination").val().trim();
+  var trainTime = $("#train-time").val().trim();
+  var frequency = $("#frequency").val().trim();
+  var convertedTime = moment(trainTime, "HH:mm A");
+  var trainTimePretty = moment(convertedTime).format("HH:mm A");
+  var nextArrival = moment(convertedTime).diff(moment(), "minutes");
 
 //Push variables to Firebase    
-    database.ref().push({
-      trainName : trainName,
-      destination : destination,
-      trainTime : trainTime,
-      frequency : frequency,
-      minsAway: minsAway
-    });
-
+  database.ref().push({
+    trainName : trainName,
+    destination : destination,
+    trainTime : trainTimePretty,
+    frequency : frequency,
+    minsAway : nextArrival
   });
+});
 
-  database.ref().on("child_added", function(snapshot){
-    var child = snapshot.val();
-
-    $(".train-list").prepend("<tr> <td>"+child.trainName+"</td> <td>"+child.destination+"</td> <td>"+child.trainTime+"</td> <td>"+child.frequency+"</td> <td>"+child.minsAway+"</td> </tr>");
-    // console.log(convertedTime);
-  });
+//Display all train times with most recent submission listed first
+database.ref().on("child_added", function(snapshot){
+  var child = snapshot.val();
+  $(".train-list").prepend("<tr> <td>"+child.trainName+"</td> <td>"+child.destination+"</td> <td>"+child.frequency+"</td> <td>"+child.trainTime+"</td> <td>"+child.minsAway+"</td> </tr>");
+});
 
 });
