e7b98f5c2583dd2362cc3c4f10768c6e69eda9df
morgan greenwalt <you@example.com>
2017-05-21 09:16:20 -0500

    buttons

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -26,6 +26,7 @@ $("#submit-button").on("click", function(event){
   var convertedTime = moment(trainTime, "HH:mm A");
   var trainTimePretty = moment(convertedTime).format("HH:mm A");
   var nextArrival = moment(convertedTime).diff(moment(), "minutes");
+  // var updateBtn = $("<button type='submit' id='updateBtn' class='btn btn-primary'>" + Update Train + " </button>");
 
 //Push variables to Firebase    
   database.ref().push({
@@ -40,7 +41,7 @@ $("#submit-button").on("click", function(event){
 //Display all train times with most recent submission listed first
 database.ref().on("child_added", function(snapshot){
   var child = snapshot.val();
-  $(".train-list").prepend("<tr> <td>"+child.trainName+"</td> <td>"+child.destination+"</td> <td>"+child.frequency+"</td> <td>"+child.trainTime+"</td> <td>"+child.minsAway+"</td> </tr>");
+  $(".train-list").prepend("<tr> <td>"+child.trainName+"</td> <td>"+child.destination+"</td> <td>"+child.frequency+"</td> <td>"+child.trainTime+"</td> <td>"+child.minsAway+"</td> <td><button type='submit' id='updateBtn' class='btn btn-default-xs'>Edit</button></td> <td><button type='submit' id='deleteBtn' class='btn btn-danger'>Clear</button></td> </tr>");
 });
 
 });
